8cc2b29f2ebdb8a5492047695335b04fdca5a69a
Paula0802 <leguizamonsuarezp@gmail.com>
2025-01-30 18:39:48 -0500

    subir graficas

--- graficas.py
+++ graficas.py
@@ -1,20 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Cargar los datos del archivo CSV
+
 df = pd.read_csv('Obesity.csv')
 
-# Crear el histograma
+
 plt.figure(figsize=(10, 6))
 plt.hist([df[df['Gender'] == 'Male']['Age'], df[df['Gender'] == 'Female']['Age']], 
          bins=20, color=['blue', 'pink'], label=['Male', 'Female'])
 
-# Añadir título y etiquetas
+
 plt.title('Histograma de Edad por Género')
 plt.xlabel('Edad')
 plt.ylabel('Frecuencia')
 plt.grid(True)
 plt.legend()
 
-# Mostrar la gráfica
+plt.savefig('histograma_edad_genero.png')
 plt.show()
